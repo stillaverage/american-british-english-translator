7fd31e8264cffeaad544d69716cfb4078cf83d33
Sean Hamilton <stillaverage@gmail.com>
2024-07-08 08:09:31 -0700

    feat: translator solution

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 'use strict';
-
 const Translator = require('../components/translator.js');
+
+const validLocales = ['american-to-british', 'british-to-american']
 
 module.exports = function (app) {
   
@@ -8,6 +9,15 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+      if (text === '') {
+        res.json({ error: 'No text to translate' })
+      } else if (!locale || text === undefined) {
+        res.json({ error: 'Required field(s) missing'})
+      } else if (validLocales.includes(locale)) {
+        res.json(translator.translate(text, locale))
+      } else {
+        res.json({ error: 'Invalid value for locale field' })
+      }
     });
 };
